f30348db5a0f3b43f0fe8418daf5e9ea18341dca
axmaxon <axmaxon@gmail.com>
2021-08-28 15:40:33 +0500

    refactor: the tests for MoviesCollection

--- spec/movies_collection_spec.rb
+++ spec/movies_collection_spec.rb
@@ -8,14 +8,16 @@ RSpec.describe MoviesCollection do
   let(:movie4) { Movie.new('Карты, деньги, два ствола', 'Ричи', '1998') }
   let(:collection) { MoviesCollection.new([movie1, movie2, movie3, movie4]) }
 
+  let(:collection_from_files) do
+    MoviesCollection.from_files(["#{__dir__}/fixtures/01.txt", "#{__dir__}/fixtures/02.txt"])
+  end
+
   describe '.from_files' do
     it 'creates an instance using data from a file' do
-      file_paths = ["#{__dir__}/fixtures/01.txt", "#{__dir__}/fixtures/02.txt"]
-
-      expect(MoviesCollection.from_files(file_paths)).to be_instance_of(MoviesCollection)
-      expect(MoviesCollection.from_files(file_paths).movies.sample).to be_instance_of(Movie)
-      expect(MoviesCollection.from_files(file_paths).movies.first.to_s).to eq('Спилберг - Терминал (2004)')
-      expect(MoviesCollection.from_files(file_paths).movies.last.to_s).to eq('Рефн - Вальгалла: Сага о викинге (2009)')
+      expect(collection_from_files).to be_instance_of(MoviesCollection)
+      expect(collection_from_files.movies).to all(be_instance_of(Movie))
+      expect(collection_from_files.movies.first.to_s).to eq('Спилберг - Терминал (2004)')
+      expect(collection_from_files.movies.last.to_s).to eq('Рефн - Вальгалла: Сага о викинге (2009)')
     end
   end
 
@@ -34,7 +36,7 @@ RSpec.describe MoviesCollection do
     end
 
     context 'when director is not exist' do
-      it 'does not return any movie' do
+      it 'returns nil' do
         expect(collection.random_movie_by_director('Кустурица')).to be_nil
       end
     end
